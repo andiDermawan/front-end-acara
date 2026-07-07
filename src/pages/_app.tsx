c1496214db4b4dfe5c5d140d774fbbf9e1838d8c
Andi Dermawan <2411016210003@mhs.ulm.ac.id>
2026-07-07 14:59:26 +0800

    feat: implement user registration page with layout and hook integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import "@/styles/globals.css";
+import { cn } from "@/utils/cn";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <NextUIProvider>
-            <Component {...pageProps} />
+            <main
+                className={cn(
+                    inter.className,
+                    "mni-w-full flex min-h-screen flex-col items-center justify-center gap-10 py-10 lg:py-0",
+                )}
+            >
+                <Component {...pageProps} />
+            </main>
         </NextUIProvider>
     );
 }
